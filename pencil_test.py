ad725690c7ea0742a873c21b04d9268445f46093
clark-lindsay <clarklindsay96@gmail.com>
2020-01-16 17:52:53 -0500

    the pencil can write words, sentences, and single characters

--- pencil_test.py
+++ pencil_test.py
@@ -3,3 +3,5 @@ import pencil
 def test_writes_text_it_is_given():
     yellow_pencil = pencil.Pencil()
     assert yellow_pencil.write('word') == 'word'
+    assert yellow_pencil.write('z') == 'z'
+    assert yellow_pencil.write('a sentence with whitespace') == 'a sentence with whitespace'
