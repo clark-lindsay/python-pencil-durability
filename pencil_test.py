2f6aa0336e3871611ec0aeffb0ad8282f1d9f895
clark-lindsay <clarklindsay96@gmail.com>
2020-01-16 18:20:00 -0500

    the pencil's point degrades when it writes non-alphanumeric characters

--- pencil_test.py
+++ pencil_test.py
@@ -6,6 +6,10 @@ def test_writes_text_it_is_given():
     assert pencil.write('z') == 'z'
     assert pencil.write('a sentence with whitespace') == 'a sentence with whitespace'
 
-def test_pencil_tip_degrades_with_use():
+def test_pencil_tip_degrades_when_writing_letters():
     pencil = Pencil(3)
     assert pencil.write('word') == 'wor '
+
+def test_pencil_tip_degrades_when_writing_non_alpha_numerics():
+    pencil = Pencil(5)
+    assert pencil.write('!@#$%^&*()') == '!@#$%     '
