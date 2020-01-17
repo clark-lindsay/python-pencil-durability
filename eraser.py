4780a769d22b9de4f91e5d8a4c60501cff744514
clark-lindsay <clarklindsay96@gmail.com>
2020-01-17 14:14:13 -0500

    erasing whitespace characters does not cost an eraser any durability

--- eraser.py
+++ eraser.py
@@ -1,10 +1,17 @@
 class Eraser:
+    def __init__(self, durability = 100):
+        self.durability = durability
+
     def erase(self, toErase, text):
         reversedText = reverse(text)
-        reversedToErase = reverse(toErase)
-        replaceWith = ' ' * len(toErase)
+        reversedToErase = reverse(toErase)[:self.durability + count_whitespace_chars(toErase)]
+        replaceWith = ' ' * len(reversedToErase)
 
+        self.durability -= len(replaceWith)
         return reverse(reversedText.replace(reversedToErase, replaceWith, 1))
 
 def reverse(text):
     return text[::-1]
+
+def count_whitespace_chars(text):
+    return len([char for char in list(text) if char.isspace()])
--- eraser_test.py
+++ eraser_test.py
@@ -1,4 +1,4 @@
-from eraser import Eraser
+from eraser import *
 
 def test_erases_the_word_it_is_given():
     eraser = Eraser()
@@ -13,3 +13,19 @@ def test_erases_only_the_last_occurence_of_given_word():
     assert firstErase == 'How much wood would a woodchuck chuck if a woodchuck could       wood?'
     assert secondErase == 'How much wood would a woodchuck chuck if a wood      could       wood?'
     assert thirdErase == 'How much wood would a woodchuck       if a wood      could       wood?'
+
+def test_eraser_does_not_erase_once_it_has_0_durability():
+    eraser = Eraser(3)
+    assert eraser.erase('Bill', 'Buffalo Bill') == 'Buffalo B   '
+
+def test_erasing_whitespace_does_not_lower_durability():
+    eraser = Eraser(10)
+    assert eraser.erase('these words', 'this word and these words') == 'this word and            '
+
+def test_reverse():
+    assert reverse('word') == 'drow'
+    assert reverse('text with spaces \n') == '\n secaps htiw txet'
+
+def test_count_whitespace_chars():
+    assert count_whitespace_chars('nospaces') == 0
+    assert count_whitespace_chars('text with spaces \n') == 4
